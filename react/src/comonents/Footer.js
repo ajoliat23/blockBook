2a6220b638de6e0a1f2e96c25005d402c6e4edd0
ajoliat23 <62565033+ajoliat23@users.noreply.github.com>
2022-09-09 23:17:34 -0400

    Create Footer.js

---
+++ react/src/comonents/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Footer = () => { //got this navbar stuff also from The Net Ninja
+    return ( 
+        <html>
+            <div className='footer'>
+                <p>important stuff here like terms of service</p>
+                
+            </div>
+        </html>
+    );
+}
+ 
+export default Footer;

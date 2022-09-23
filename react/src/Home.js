cb14a9acd620c2290bd36eaa9bd58c839cbabd31
Zachtmac1 <81589146+Zachtmac1@users.noreply.github.com>
2022-09-23 17:17:42 -0400

    feat: adds nft search webpage and related files

--- react/src/Home.js
+++ react/src/Home.js
@@ -3,27 +3,27 @@ import Navbar from './components/Navbar';
 
 
 const Home = () => {
-    return ( 
+    return (
         <html >
             <div className='midCol'>
                 <h1>Welcome to Block Book</h1>
                 <div className='trade'>
                     <div className='currency'>
-                        <img src="usd.jpg" width="40px" className='currencyImg'/>
+                        <img src="usd.jpg" width="40px" className='currencyImg' />
                         <label for="usd">USD</label>
                         <input type="text" id="usd" className='tradeInput'></input>
                     </div>
-                    <img src="switchArrows.jpg"/>
+                    <img src="switchArrows.jpg" />
                     <div className='currency'>
-                        <img src="sol.jpg" width="40px" className='currencyImg'/>
+                        <img src="sol.jpg" width="40px" className='currencyImg' />
                         <label for="sol">SOL</label>
                         <input type="text" id="sol" className='tradeInput'></input>
                     </div>
                 </div>
             </div>
-            
+
         </html>
-     );
+    );
 }
- 
+
 export default Home;

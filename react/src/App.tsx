5a88afcf794cbc80add6ba728f875c4bbec01fcf
Alixandria Joliat <ajoliat23@jcu.edu>
2022-10-07 01:05:20 -0400

    wallet-funcionality fix NFT files by reverting to .js

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,102 +1,86 @@
+
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Home from './Home';
 import About from './About';
-import Wallet from './Wallet';
 import Connect from './Connect';
 import NFTSearch from './NFTSearch';
+import Home from './Home';
 import Footer from './components/Footer';
-import MyComponent from './components/MyComponent';
-import React, { useMemo } from 'react';
-import { BrowserRouter, Route, Switch} from 'react-router-dom';
-
-//---------------------Wallet imports---------------------------------------
-import { clusterApiUrl } from '@solana/web3.js';
+import React, { FC, useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
-    GlowWalletAdapter,
-    LedgerWalletAdapter,
-    PhantomWalletAdapter,
-    SlopeWalletAdapter,
-    SolflareWalletAdapter,
-    SolletExtensionWalletAdapter,
-    SolletWalletAdapter,
-    TorusWalletAdapter,
+  GlowWalletAdapter,
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import {
+    WalletModalProvider,
+    WalletDisconnectButton,
+    WalletMultiButton
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
-import './App.css';
-
-// import the styles
+// Default styles that can be overridden by your app
 require('@solana/wallet-adapter-react-ui/styles.css');
-//--------------------------------wallet imports end------------------------------------------------
 
-function App() {
- // you can use Mainnet, Devnet or Testnet here
- const network = WalletAdapterNetwork.Mainnet;
- const endpoint = useMemo(() => clusterApiUrl(network), [network]);
- // initialise all the wallets you want to use
- const wallets = useMemo(
-     () => [
-         new PhantomWalletAdapter(),
-         new GlowWalletAdapter(),
-         new SlopeWalletAdapter(),
-         new SolflareWalletAdapter({ network }),
-         new TorusWalletAdapter(),
-         //new LedgerWalletAdapter(),
-         new SolletExtensionWalletAdapter(),
-         new SolletWalletAdapter(),
-     ],
-     [network]
- );
+export const Wallet: FC = () => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Devnet;
 
-    
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  return (
-    
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
+    const wallets = useMemo(
+        () => [
+            /**
+             * Select the wallets you wish to support, by instantiating wallet adapters here.
+             *
+             * Common adapters can be found in the npm package `@solana/wallet-adapter-wallets`.
+             * That package supports tree shaking and lazy loading -- only the wallets you import
+             * will be compiled into your application, and only the dependencies of wallets that
+             * your users connect to will be loaded.
+             */
+             new PhantomWalletAdapter(),
+             
+             new GlowWalletAdapter(),
+             new SlopeWalletAdapter(),
+             //new SolflareWalletAdapter({ network }),
+             new TorusWalletAdapter(),
+             //new LedgerWalletAdapter(),
+             new SolletExtensionWalletAdapter(),
+             new SolletWalletAdapter(),
            
-          <BrowserRouter>
-          <div className ="App">
-            <div className='nav'>
-              <Navbar />
-              <MyComponent/>
-                          
-            </div>
-            
-            
-            <div className = "content"> 
-            <Switch> 
-                <Route path = "/home">
-                  <Home/>
-                </Route>
-                <Route path = "/wallet">
-                  <Wallet/>
-                </Route>
-                <Route path = "/connect">
-                  <Connect/>
-                </Route>
-                <Route path = "/about">
-                  <About/>
-                </Route>
-                <Route path="/nftsearch">
-                  <NFTSearch />
-                </Route>
-            </Switch>
-            
-            </div>
+        ],
+        []
+    );
 
-            <Footer/>
-          </div>
-        </BrowserRouter>
-        </WalletModalProvider>
-      </WalletProvider>
-  </ConnectionProvider>
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>
+                    <BrowserRouter>
+                    <div className='nav'>
+                        <Navbar/>
+                       <WalletMultiButton /> 
+                    </div>
+                    <Routes>
+                        <Route path="/" element = {<Home/>}/>
+                        <Route path="about" element = {<About/>}/>
+                        <Route path="connect" element = {<Connect/>}/>
+                        <Route path="nftsearch" element = {<NFTSearch/>}/>
+                    </Routes>
+                    <Footer/>  
+                    </BrowserRouter>
+                  </WalletModalProvider>
+               </WalletProvider>
+        </ConnectionProvider>
+    );
+};
 
-  )
-}
-
-export default App;
-
+export default Wallet;

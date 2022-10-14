e7d255dd2c62115af3f3eb45def65903bebc35cf
Alixandria Joliat <ajoliat23@jcu.edu>
2022-10-14 02:33:42 -0400

    testing scroll css

--- react/src/components/Scroll.tsx
+++ react/src/components/Scroll.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props: any) => {
     return (
-        <div style={{ overflowY: 'scroll', height: '70vh' }}>
+        <div style={{ overflowY: 'scroll', height: '78vh' }}>
             {props.children}
         </div>
     );

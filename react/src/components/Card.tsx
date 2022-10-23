4db1c32dc3b99f27553c2801add3c90816c7c063
Alixandria Joliat <ajoliat23@jcu.edu>
2022-10-23 17:05:48 -0400

    Fix NFT format

--- react/src/components/Card.tsx
+++ react/src/components/Card.tsx
@@ -4,21 +4,17 @@ import StackGrid from "react-stack-grid";
 function Card({ NFT }: { NFT: any }) {
     return (
         
-        <div className="trade">
-            <StackGrid //supposed to give things a gallery type of view, but it's not working
-                columnWidth={100}
-                duration={0}
-            >
-                <div className="nftBox">
-                    <img className="nftimg" width="auto" alt={NFT.name} src={process.env.PUBLIC_URL + NFT.imgPath} />
-                </div>
-                
-                <div>
-                    <h2>{NFT.name}</h2>
-                    <p>{NFT.description}</p>
-                    <p>Owner: {NFT.owner}</p>
-                </div>
-            </StackGrid>
+        <div className="">
+               
+            <div className="nftBox">
+                <img className="nftimg" width="auto" alt={NFT.name} src={process.env.PUBLIC_URL + NFT.imgPath} />
+                    <div>
+                        <h2>{NFT.name}</h2>
+                        <p>{NFT.description}</p>
+                        <p>Owner: {NFT.owner}</p>
+                    </div>
+            </div>
+            
         </div>
         
     );

5a727d3ca1e10e3d3eea6eb924df53000ae09d52
Tyler Smith <tpsmith23@jcu.edu>
2022-10-23 19:32:44 -0400

    Reversal

--- react/src/Home.tsx
+++ react/src/Home.tsx
@@ -1,68 +1,29 @@
 import React, { useState } from 'react';
 import Navbar from './components/Navbar';
-import {GetPrice} from './components/SolanaPrice'
-
-var usdFill = null;
-var solFill = null;
-var convertRate:number = GetPrice();
-
-function Home(){
-
-    function buttonPressed1() {
-        var usd = (document.getElementById("usd") as HTMLInputElement).value;
-         //this is a place holder until api / live connection works
-        var sol = usd / convertRate;
 
 
-        (document.getElementById("usd") as HTMLInputElement).value = "";
-        (document.getElementById("sol") as HTMLInputElement).placeholder = sol;
-        (document.getElementById("usd") as HTMLInputElement).placeholder = ""
-
-        return;
-    }
-
-    function buttonPressed2() {
-        var sol = document.getElementById("sol").value;
-        var convertRate = 35; //this is a place holder until api / live connection works
-        var usd = sol * convertRate;
-
-        (document.getElementById("sol") as HTMLInputElement).value = "";
-        (document.getElementById("usd") as HTMLInputElement).placeholder = usd;
-        (document.getElementById("sol") as HTMLInputElement).placeholder = ""
-
-        return;
-    }
-
-    function purchase() {
-        alert("Purchase");
-    }
-
-
-
-
-    return ( 
-        <html>
+function Home() {
+    return (
+        <html >
             <div className='midCol'>
                 <h1>Welcome to Block Book</h1>
                 <div className='trade'>
                     <div className='currency'>
                         <img src="usd.jpg" width="40px" className='currencyImg' />
                         <label >USD</label>
-                        <input type="text" id="usd" className='tradeInput' placeholder={usdFill}/>
-                        <button onClick={buttonPressed1} className="button1">Convert to SOL</button>
+                        <input type="text" id="usd" className='tradeInput'></input>
                     </div>
-                    <img src="switch.png" width="40px" height="40px"/>
+                    <img src="switchArrows.jpg" />
                     <div className='currency'>
-                        <img src="sol.jpg" width="40px" className='currencyImg'/>
+                        <img src="sol.jpg" width="40px" className='currencyImg' />
                         <label >SOL</label>
-                        <input type="text" id="sol" className='tradeInput' placeholder={solFill}/>
-                        <button onClick={buttonPressed2} className="button1">Convert to USD</button>
+                        <input type="text" id="sol" className='tradeInput'></input>
                     </div>
-                    <button onClick={purchase} className="button2">Purchase</button>
                 </div>
             </div>
+
         </html>
-     )
+    )
 }
 
 export default Home;
